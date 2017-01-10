571f3f50183f09d4e0bd2047fac76fedf7c0a3e4
Hoang Huynh Man <hoanghuynhman@gmail.com>
2017-01-10 09:08:06 +0700

    Init project

---
+++ app/src/main/java/com/framgia/mixrecorder/ui/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.framgia.mixrecorder.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.framgia.mixrecorder.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
