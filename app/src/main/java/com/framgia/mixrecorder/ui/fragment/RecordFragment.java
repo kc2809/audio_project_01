01a125208a33b468c1ebcc489dc47921ea8038cf
Hoang Huynh Man <hoanghuynhman@gmail.com>
2017-01-13 09:11:31 +0700

    make record, pause, stop functions

--- app/src/main/java/com/framgia/mixrecorder/ui/fragment/RecordFragment.java
+++ app/src/main/java/com/framgia/mixrecorder/ui/fragment/RecordFragment.java
@@ -5,10 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.framgia.mixrecorder.R;
+import com.framgia.mixrecorder.utils.RecordHelper;
 
-public class RecordFragment extends Fragment {
+public class RecordFragment extends Fragment implements View.OnClickListener {
+    private Button mButtonRecord;
+    private Button mButtonStop;
+    private boolean mIsRecording;
+    private boolean mIsNewRecord = true;
+    private boolean mIsResponsable = true;
+    private RecordHelper mRecordHelper;
+
     public RecordFragment() {
         // Required empty public constructor
     }
@@ -16,7 +25,91 @@ public class RecordFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_audio, container, false);
+        return inflater.inflate(R.layout.fragment_record, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstance) {
+        super.onActivityCreated(savedInstance);
+        mButtonRecord = (Button) getView().findViewById(R.id.btn_record);
+        mButtonStop = (Button) getView().findViewById(R.id.btn_stop);
+        mButtonRecord.setOnClickListener(this);
+        mButtonStop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        processStopEvent();
+    }
+
+    private void processRecordEvent() {
+        if (!mIsResponsable) return;
+        if (mIsNewRecord) newAudio();
+        if (!mIsRecording) record();
+        else pause();
+    }
+
+    private void processStopEvent() {
+        if (!mIsResponsable || mIsNewRecord) return;
+        if (mIsRecording) pause();
+        releaseAudio();
+    }
+
+    private void newAudio() {
+        mRecordHelper = new RecordHelper();
+        mIsNewRecord = false;
+        mButtonStop.setBackgroundResource(R.drawable.ic_stop_active);
+    }
+
+    private void releaseAudio() {
+        mRecordHelper = null;
+        mIsNewRecord = true;
+        mButtonStop.setBackgroundResource(R.drawable.ic_stop_inactive);
+    }
+
+    private void record() {
+        mRecordHelper.record(new RecordHelper.OnRecordListener() {
+            @Override
+            public void onPreRecord() {
+                mIsResponsable = false;
+            }
+
+            @Override
+            public void onPostRecord() {
+                mIsResponsable = true;
+            }
+        });
+        mIsRecording = true;
+        mButtonRecord.setBackgroundResource(R.drawable.ic_pause_active);
+    }
+
+    private void pause() {
+        mRecordHelper.stop(new RecordHelper.OnStopListener() {
+            @Override
+            public void onPreStop() {
+                mIsResponsable = false;
+            }
+
+            @Override
+            public void onPostStop() {
+                mIsResponsable = true;
+            }
+        });
+        mIsRecording = false;
+        mButtonRecord.setBackgroundResource(R.drawable.ic_record_active);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_record:
+                processRecordEvent();
+                break;
+            case R.id.btn_stop:
+                processStopEvent();
+                break;
+            default:
+        }
     }
 }
---
+++ app/src/main/java/com/framgia/mixrecorder/utils/RecordHelper.java
@@ -0,0 +1,167 @@
+package com.framgia.mixrecorder.utils;
+
+import android.media.MediaRecorder;
+import android.os.AsyncTask;
+import android.os.Environment;
+
+import com.coremedia.iso.boxes.Container;
+import com.googlecode.mp4parser.FileDataSourceImpl;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.Track;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
+import com.googlecode.mp4parser.authoring.tracks.AppendTrack;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.Channels;
+import java.nio.channels.WritableByteChannel;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * Created by hoang on 1/12/2017.
+ */
+public class RecordHelper {
+    private static final int FILE_BUFFER_SIZE = 1024;
+    private static final String TEMPORARY = "/temporary";
+    private String mTemporaryPath;
+    private String mFilePath;
+    private MediaRecorder mRecorder;
+
+    public RecordHelper() {
+        mFilePath = getAbsolutePath() + Constant.FOLDER + getCurrentTime() + Constant.AUDIO_TYPE;
+        mTemporaryPath = getAbsolutePath() + TEMPORARY + Constant.AUDIO_TYPE;
+    }
+
+    private void append(String mainFileName, String anotherFileName) {
+        try {
+            File targetFile = new File(mainFileName);
+            File anotherFile = new File(anotherFileName);
+            if (targetFile.exists() && targetFile.length() > 0) {
+                String tmpFileName = mainFileName + ".tmp";
+                appendWithNewFile(mainFileName, anotherFileName, tmpFileName);
+                copyFile(mainFileName, tmpFileName);
+                anotherFile.delete();
+            } else {
+                targetFile.getParentFile().mkdirs();
+                targetFile.createNewFile();
+                copyFile(mainFileName, anotherFileName);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void appendWithNewFile(final String firstFile, final String secondFile,
+                                   final String newFile) throws IOException {
+        final Movie movieA = MovieCreator.build(new FileDataSourceImpl(secondFile));
+        final Movie movieB = MovieCreator.build(new FileDataSourceImpl(firstFile));
+        final Movie finalMovie = new Movie();
+        final List<Track> movieOneTracks = movieA.getTracks();
+        final List<Track> movieTwoTracks = movieB.getTracks();
+        for (int i = 0; i < movieOneTracks.size() || i < movieTwoTracks.size(); i++) {
+            finalMovie.addTrack(new AppendTrack(movieTwoTracks.get(i), movieOneTracks.get(i)));
+        }
+        final Container container = new DefaultMp4Builder().build(finalMovie);
+        final FileOutputStream fos = new FileOutputStream(new File(String.format(newFile)));
+        final WritableByteChannel bb = Channels.newChannel(fos);
+        container.writeContainer(bb);
+        fos.close();
+    }
+
+    private void copyFile(String destination, String from) throws IOException {
+        FileInputStream in = new FileInputStream(from);
+        FileOutputStream out = new FileOutputStream(destination);
+        byte[] buf = new byte[FILE_BUFFER_SIZE];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
+        new File(from).delete();
+    }
+
+    private String getCurrentTime() {
+        return new SimpleDateFormat(Constant.TIME_FORMAT).format(Calendar.getInstance().getTime());
+    }
+
+    private String getAbsolutePath() {
+        return Environment.getExternalStorageDirectory().getAbsolutePath();
+    }
+
+    public void record(OnRecordListener onRecordListener) {
+        new RecordAsyncTask().execute(onRecordListener);
+    }
+
+    public void stop(OnStopListener onStopListener) {
+        new StopAsyncTask().execute(onStopListener);
+    }
+
+    public interface OnRecordListener {
+        void onPreRecord();
+        void onPostRecord();
+    }
+
+    public interface OnStopListener {
+        void onPreStop();
+        void onPostStop();
+    }
+
+    private class RecordAsyncTask extends AsyncTask<OnRecordListener, Void, Void> {
+        private OnRecordListener mOnRecordListener;
+
+        @Override
+        protected Void doInBackground(OnRecordListener... onRecordListeners) {
+            mOnRecordListener = onRecordListeners[0];
+            onRecordListeners[0].onPreRecord();
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setOutputFile(mTemporaryPath);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            try {
+                mRecorder.prepare();
+                mRecorder.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            mOnRecordListener.onPostRecord();
+        }
+    }
+
+    private class StopAsyncTask extends AsyncTask<OnStopListener, Void, Void> {
+        private OnStopListener mOnStopListener;
+
+        @Override
+        protected Void doInBackground(OnStopListener... onStopListeners) {
+            mOnStopListener = onStopListeners[0];
+            onStopListeners[0].onPreStop();
+            try {
+                mRecorder.stop();
+                mRecorder.release();
+                mRecorder = null;
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+            append(mFilePath, mTemporaryPath);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            mOnStopListener.onPostStop();
+        }
+    }
+}
